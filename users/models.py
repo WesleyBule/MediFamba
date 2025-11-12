a18f5286370c20d7fca6e4f2321d758e99b67954
Gian /Dev <bulewesley7@gmail.com>
2025-11-12 16:35:25 +0200

    fix: Templates Issues

--- users/models.py
+++ users/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-
 class Doctor(models.Model):
     user = models.OneToOneField(User ,on_delete=models.CASCADE)
     name = models.CharField(max_length=255, default="Dr. ")
@@ -12,6 +11,7 @@ class Doctor(models.Model):
 
     def __str__(self):
         return self.name
+
 
 class Patient(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
