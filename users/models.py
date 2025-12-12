8c76d846568f0d5e974daf7f8d378ae41f9826d6
WesleyBule <bulewesley7@gmail.com>
2025-12-12 22:45:01 +0200

    feat: Patient details modal for doctors

--- users/models.py
+++ users/models.py
@@ -4,6 +4,8 @@ from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import User , AbstractUser
 
+from datetime import date
+
 class CustomUser(AbstractUser):
     id = models.UUIDField(primary_key=True,default=uuid.uuid4, editable=False)
 
@@ -12,6 +14,8 @@ class Doctor(models.Model):
     id = models.UUIDField(primary_key=True,default=uuid.uuid4, editable=False)
     user = models.OneToOneField(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
     name = models.CharField(max_length=255, default="")
+    firstname = models.CharField(max_length=255, default="")
+    lastname = models.CharField(max_length=255, default="")
     birthDate = models.DateField(default="2000-01-01")
     speciality = models.CharField(max_length=120)
     residence = models.CharField(max_length=50, default="")
@@ -29,9 +33,17 @@ class Patient(models.Model):
     lastname = models.CharField(max_length=255, default="")
     residence = models.CharField(max_length=50, default="")
     phoneNumber = models.CharField(max_length=14, unique=True, default="+258 ")
-    gender = models.CharField(max_length=8, choices=[('M',"Male"),('F','Female')], default="")
+    gender = models.CharField(max_length=8, choices=[('male',"Male"),('female','Female')], default="")
     birthDate = models.DateField()
     profile = models.ImageField(blank=True, null=True , default='defaultpfp.jpg')
+
+    @property
+    def age(self):
+        today = date.today()
+        return (
+            today.year - self.birthDate.year
+            - ((today.month, today.day) < (self.birthDate.month, self.birthDate.day) )
+        )
 
     def __str__(self):
         return f"{self.firstname} {self.lastname}"
